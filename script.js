06314eda1edf285ce79ab032ab5f483985e22ff8
Aravind Chinna <151151715+aravi143@users.noreply.github.com>
2024-04-01 14:57:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let s = arr.sort;
+	let n = s.lenght;
+	let sum = 0;
+	for(let i= 0; i <= n -1 ; i++){
+		if(arr[i] > 0){
+			sum = 2 * arr[i];
+		}
+		return sum;
+	} 
 }
 
 module.exports=mincost;
